5f9eeedb349084760d187371c0be5e8f4be40f46
ErRayn <yasintopcu3300@gmail.com>
2023-04-06 00:44:45 +0300

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by ErRayn on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
